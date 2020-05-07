55bb98e23417605f10f20b4f5dc6a8bf75b1995f
DamianKuich <damian.kuich@hotmail.com>
2020-05-07 20:02:53 +0200

    Added simple model for future tasks that will be added to database.

--- djsr/authentication/models.py
+++ djsr/authentication/models.py
@@ -6,3 +6,15 @@ from django.db import models
 class CustomUser(AbstractUser):
     fav_color = models.CharField(blank=True, max_length=120)
 # Create your models here.
+
+
+class Tasks(models.Model):
+    Text = models.CharField(max_length=500)
+    add_date = models.DateField()
+    is_open = models.IntegerField()
+    author = models.CharField(max_length=100)
+    true_false = models.IntegerField()
+    abcd = models.IntegerField()
+    difficulty = models.IntegerField()
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
--- djsr/authentication/urls.py
+++ djsr/authentication/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import url
 from rest_framework_simplejwt import views as jwt_views
 from .views import CustomUserCreate, HelloWorldView, LogoutAndBlacklistRefreshTokenForUserView
 
@@ -8,4 +9,5 @@ urlpatterns = [
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('hello/', HelloWorldView.as_view(), name='hello_world'),
     path('blacklist/', LogoutAndBlacklistRefreshTokenForUserView.as_view(), name='blacklist'),
+    url(r'^api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
 ]
