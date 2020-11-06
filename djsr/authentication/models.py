8e2048b3768826a9ae6cea193ec599e57130fdf9
Koffalox <35637508+Koffalox@users.noreply.github.com>
2020-11-06 21:51:00 +0100

    change vol2

--- djsr/authentication/models.py
+++ djsr/authentication/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from jsonfield import JSONField
 from django.db import models
 
 from django_mysql.models import ListCharField
