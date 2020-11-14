40b075ed1ebe4dd0dad9d53d330ee337eeb29eee
Koffalox <35637508+Koffalox@users.noreply.github.com>
2020-11-14 02:01:18 +0100

    db fix

--- djsr/authentication/serializers.py
+++ djsr/authentication/serializers.py
@@ -89,7 +89,7 @@ class TaskSerializer(serializers.ModelSerializer):
     # level = serializers.IntegerField(required=True)
     # answer = serializers.CharField(required=True)
     skill = SkillSerializer(many=True)
-    answers = AnswersSerializer(many=True)
+    answers = AnswersSerializer(many=False)
     image = ImageSerializer(many=True,required=False)
 
 
