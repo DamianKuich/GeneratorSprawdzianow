adb5e434f8b2c3d0089c830bbb6e117b3bde9613
pafcioo123 <l.kubiak2222@gmail.com>
2020-12-10 19:51:27 +0100

    frond end- remove static files

--- djsr/staticfiles/frontend/public/17.main.598a624d9d12.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{1421:function(n,e,o){"use strict";o.r(e);var t=o(1),a=o.n(t);e.default=function(n){return a.a.createElement("div",null,a.a.createElement("span",null,"Pomyślnie utworzono konto użytkownika"))}}}]);
