badb7d4436a90207b893c3c3dd02f1dcfefc6fbf
Damian Kuich <43881268+DamianKuich@users.noreply.github.com>
2020-12-18 19:56:58 +0100

    Token deletion.

--- clock.py
+++ clock.py
@@ -6,5 +6,6 @@ sched = BlockingScheduler()
 @sched.scheduled_job('interval', minutes=1)
 def scheduled_job():
     subprocess.run(["heroku run python djsr/manage.py flushexpiredtokens"])
+    print("dzialam")
 
 sched.start()
