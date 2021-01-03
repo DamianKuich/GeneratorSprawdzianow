ff49a6614e92eeeac0086bfd310df5463ae5d306
Damian Kuich <43881268+DamianKuich@users.noreply.github.com>
2021-01-03 19:15:09 +0100

    DB for user image.

--- djsr/djsr/settings.py
+++ djsr/djsr/settings.py
@@ -120,8 +120,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'genespr',
-        'USER': 'myroot',
-        'PASSWORD': 'Papaj2137',
+        'USER': 'root',
+        'PASSWORD': 'ziomalek1998',
         'HOST': 'localhost',
         'PORT': '3306',
     }
