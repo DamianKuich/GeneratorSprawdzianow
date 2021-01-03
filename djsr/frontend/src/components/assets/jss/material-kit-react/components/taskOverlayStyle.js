b0a801e9d7a99633d5cea523254c666bbc2226c7
pafcioo123 <l.kubiak2222@gmail.com>
2021-01-03 23:19:25 +0000

    overlay uses classnames update

--- djsr/frontend/src/components/assets/jss/material-kit-react/components/taskOverlayStyle.js
+++ djsr/frontend/src/components/assets/jss/material-kit-react/components/taskOverlayStyle.js
@@ -14,10 +14,25 @@ const taskOverlayStyle = {
     padding: "0px",
   },
   activeRoot: {
-    border: "1px solid black",
+    border: `1px solid ${primaryColor}`,
     margin: "-2px -13px",
     padding: "1px 12px",
   },
+  menuContainer: {
+    position: "absolute",
+    display: "flex",
+    color: "white",
+  },
+  menuLeftLeaf: {
+    border: "solid transparent",
+    borderRight: `solid ${primaryColor}`,
+    borderWidth: "22px 12px 0 0",
+  },
+  menuRightLeaf: {
+    border: "solid transparent",
+    borderLeft: `solid ${primaryColor}`,
+    borderWidth: "22px 0 0 12px",
+  },
 };
 
-export default taskOverlayStyle
+export default taskOverlayStyle;
