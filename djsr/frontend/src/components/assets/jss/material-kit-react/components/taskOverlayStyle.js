91d15e2c3ef916eaed7364d33e70a0be90e23498
pafcioo123 <l.kubiak2222@gmail.com>
2021-01-03 22:12:09 +0000

    overlay uses classnames

---
+++ djsr/frontend/src/components/assets/jss/material-kit-react/components/taskOverlayStyle.js
@@ -0,0 +1,23 @@
+import {
+  primaryColor,
+  warningColor,
+  dangerColor,
+  successColor,
+  infoColor,
+  roseColor,
+} from "../../material-kit-react";
+
+const taskOverlayStyle = {
+  root: {
+    position: "relative",
+    margin: "0px",
+    padding: "0px",
+  },
+  activeRoot: {
+    border: "1px solid black",
+    margin: "-2px -13px",
+    padding: "1px 12px",
+  },
+};
+
+export default taskOverlayStyle
--- djsr/frontend/static/frontend/public/17.main.js
+++ djsr/frontend/static/frontend/public/17.main.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{1487:function(n,e,o){"use strict";o.r(e);var t=o(1),a=o.n(t);e.default=function(n){return a.a.createElement("div",null,a.a.createElement("span",null,"Pomyślnie utworzono konto użytkownika"))}}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[17],{1488:function(n,e,o){"use strict";o.r(e);var t=o(1),a=o.n(t);e.default=function(n){return a.a.createElement("div",null,a.a.createElement("span",null,"Pomyślnie utworzono konto użytkownika"))}}}]);
