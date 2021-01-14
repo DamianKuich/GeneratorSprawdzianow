02f1790f901f09ea1d548fe716a2afacc72da6a8
pafcioo123 <l.kubiak2222@gmail.com>
2021-01-14 23:31:32 +0100

    create authimg

---
+++ djsr/frontend/src/components/ExamEditorSubComponents/AuthImage.js
@@ -0,0 +1,16 @@
+import React, {useState,useEffect} from 'react';
+
+const AuthImage = (props) => {
+    const {src}=props
+    const [blob,setBlob]=useState(null);
+    useEffect(()=>{
+
+    },);
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default AuthImage;
