f03453180b7ebd884b7d93c2a7bbdca57e0076da
pafcioo123 <l.kubiak2222@gmail.com>
2021-01-15 00:20:33 +0100

    Added AuthImage.js handler (compiled)

--- djsr/frontend/static/frontend/public/7.main.js
+++ djsr/frontend/static/frontend/public/7.main.js
@@ -267,6 +267,77 @@ var AddTaskWithDropdownMenu = function AddTaskWithDropdownMenu(_ref) {
 
 /***/ }),
 
+/***/ "./djsr/frontend/src/components/ExamEditorSubComponents/AuthImage.js":
+/*!***************************************************************************!*\
+  !*** ./djsr/frontend/src/components/ExamEditorSubComponents/AuthImage.js ***!
+  \***************************************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _axiosAPI__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../axiosAPI */ "./djsr/frontend/src/components/axiosAPI.js");
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+
+
+
+
+var AuthImage = function AuthImage(props) {
+  var src = props.src,
+      rest = _objectWithoutProperties(props, ["src"]);
+
+  var _useState = Object(react__WEBPACK_IMPORTED_MODULE_0__["useState"])(null),
+      _useState2 = _slicedToArray(_useState, 2),
+      blob = _useState2[0],
+      setBlob = _useState2[1];
+
+  Object(react__WEBPACK_IMPORTED_MODULE_0__["useEffect"])(function () {
+    if (!!blob) {
+      URL.revokeObjectURL(blob);
+    }
+
+    _axiosAPI__WEBPACK_IMPORTED_MODULE_1__["default"].get("/user/image/".concat(src), {
+      method: "GET",
+      responseType: "blob"
+    }).then(function (response) {
+      var imgBlob = new Blob([response.data]);
+      var imgUrl = URL.createObjectURL(imgBlob);
+      setBlob(imgUrl);
+    });
+    return function () {
+      if (!!blob) {
+        console.log("revoking img", src);
+        URL.revokeObjectURL(blob);
+      }
+    };
+  }, [src]);
+  return !!blob ? /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", _extends({
+    src: blob
+  }, rest)) : null;
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (AuthImage);
+
+/***/ }),
+
 /***/ "./djsr/frontend/src/components/ExamEditorSubComponents/CollapseMenuContainer.js":
 /*!***************************************************************************************!*\
   !*** ./djsr/frontend/src/components/ExamEditorSubComponents/CollapseMenuContainer.js ***!
@@ -2611,7 +2682,9 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _material_ui_icons_DeleteForever__WEBPACK_IMPORTED_MODULE_15___default = /*#__PURE__*/__webpack_require__.n(_material_ui_icons_DeleteForever__WEBPACK_IMPORTED_MODULE_15__);
 /* harmony import */ var _EditTaskImages__WEBPACK_IMPORTED_MODULE_16__ = __webpack_require__(/*! ./EditTaskImages */ "./djsr/frontend/src/components/ExamEditorSubComponents/EditTaskImages.js");
 /* harmony import */ var _EditTaskAnswerSpace__WEBPACK_IMPORTED_MODULE_17__ = __webpack_require__(/*! ./EditTaskAnswerSpace */ "./djsr/frontend/src/components/ExamEditorSubComponents/EditTaskAnswerSpace.js");
+/* harmony import */ var _AuthImage__WEBPACK_IMPORTED_MODULE_18__ = __webpack_require__(/*! ./AuthImage */ "./djsr/frontend/src/components/ExamEditorSubComponents/AuthImage.js");
 function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
 
 
 
@@ -2734,8 +2807,8 @@ var TaskWithOverlay = function TaskWithOverlay(props) {
           // xl={1}
           // lg={1}
 
-        }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("img", {
-          src: "".concat(window.location.origin, "/api/user/image/").concat(image),
+        }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_AuthImage__WEBPACK_IMPORTED_MODULE_18__["default"], {
+          src: image,
           style: {
             maxHeight: "5cm",
             maxWidth: "100%",
